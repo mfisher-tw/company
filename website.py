f93f9cb1a9175db39c364ed2f56b2c5c8b11edce
nperalta-tw <nperalta@telware.com>
2020-10-23 10:28:38 -0400

    update tasks

--- website.py
+++ website.py
@@ -7,5 +7,10 @@ def main():
     response = render_template('index.html')
     return response
 
+@app.route("/cheatsheet")
+def cheatsheet():
+    response = render_template('cheatsheet.html')
+    return response
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
